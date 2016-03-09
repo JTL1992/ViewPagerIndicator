32985ecf18b9ad76dbfc82c18f82d39ef1376ca2
JTL1992 <talentjiang@yeah.net>
2016-03-09 02:25:59 +0100

    增加了一个样板

--- app/src/main/java/com/alizbeo/myviewpagerdemo/MainActivity.java
+++ app/src/main/java/com/alizbeo/myviewpagerdemo/MainActivity.java
@@ -1,59 +1,37 @@
 package com.alizbeo.myviewpagerdemo;
 
-import android.app.ActionBar;
-import android.support.v4.view.ViewPager;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.RelativeLayout;
-
-import com.shizhefei.view.indicator.Indicator;
-import com.shizhefei.view.indicator.IndicatorViewPager;
-
-import java.util.zip.Inflater;
-
-import static com.alizbeo.myviewpagerdemo.R.id.guide_indicator;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
-    private IndicatorViewPager indicatorViewPager;
+/**
+ * 主程序负责导入各种样板
+ */
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+    private Button btnGuide,btnMoreTab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_guide);
-        ViewPager viewPager = (ViewPager) findViewById(R.id.guide_viewPager);
-        Indicator indicator = (Indicator) findViewById(guide_indicator);
-        indicatorViewPager = new IndicatorViewPager(indicator,viewPager);
-        indicatorViewPager.setAdapter(adapter);
-
+        setContentView(R.layout.activity_main);
     }
-    private IndicatorViewPager.IndicatorViewPagerAdapter adapter = new IndicatorViewPager.IndicatorViewPagerAdapter() {
-        private int[] imageIds = {R.mipmap.p1,R.mipmap.p2,R.mipmap.p3,R.mipmap.p4};
-        @Override
-        public int getCount() {
-            return imageIds.length;
-        }
 
-        @Override
-        public View getViewForTab(int i, View view, ViewGroup viewGroup) {
-            LayoutInflater layoutInflater =  LayoutInflater.from(getApplicationContext());
-            if (null == view){
-               view = layoutInflater.inflate(R.layout.tab_guide,viewGroup,false);
-            }
-            return view;
+    /**点击事件响应
+     * @param v 点击的view
+     */
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        Intent intent = new Intent();
+        switch (id){
+            case R.id.btn_guide :
+                intent.setClass(this, GuideActivity.class);
+                startActivity(intent);
+            case R.id.btn_moretab :
+                intent.setClass(this, GuideActivity.class);
+                startActivity(intent);
         }
-
-        @Override
-        public View getViewForPage(int i, View view, ViewGroup viewGroup) {
-            if (null == view){
-                view = new View(getApplicationContext());
-                view.setLayoutParams(new ViewGroup.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
-            }
-            view.setBackgroundResource(imageIds[i]);
-            return view;
-        }
-    };
-
+    }
 }
